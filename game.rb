553e3d74a34e572dd6e11c6d4fdeb33557a781e9
supersakana <zacwilliamson@icloud.com>
2022-03-22 10:51:51 -0600

    created Game class with method that fetches a valid word from the txt file

--- game.rb
+++ game.rb
@@ -1,7 +1,27 @@
 # frozen_string_literal: true
 
-word_file = File.open('google-10000-english-no-swears.txt')
+require_relative 'intro'
 
-words = word_file.readlines.map(&:chomp)
+# when a new game is created
+class Game
+  include Intro
 
-puts words
+  def initialize
+    @guess_word = nil
+  end
+
+  def game
+    puts instructions
+    create_word
+    puts @guess_word
+  end
+
+  def create_word
+    word_file = File.open('google-10000-english-no-swears.txt')
+    words = word_file.readlines.map(&:chomp).select { |word| word.length > 4 && word.length < 13 }
+    @guess_word = words.sample
+  end
+end
+
+start = Game.new
+start.game
--- intro.rb
+++ intro.rb
@@ -1,6 +1,22 @@
 # frozen_string_literal: true
 
-class Intro
-   
-    
+# displayed at the begining of the game.
+module Intro
+  def instructions
+    <<~HEREDOC
+
+      Welcome to Hangman!
+
+      This is a 1 player game.
+
+      The goal of the player is to break the CPU code with only 6 chances.
+
+      If you guess correctly, your chances remain.
+
+      If you guess incorrectly, you deduct 1 chance.
+
+      Let's begin!
+
+    HEREDOC
+  end
 end
